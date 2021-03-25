0f189a8d847f730b470e53b88fd6756f73da56db
kygm <rickev20@gmail.com>
2021-03-25 02:52:25 -0500

    Version 1.00

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class test
+{
+  public static void main(String[] args)
+  {
+    Scanner kb = new Scanner(System.in);
+    
+    System.out.println("Test");
+    
+    
+    kb.next();
+  }
+}
